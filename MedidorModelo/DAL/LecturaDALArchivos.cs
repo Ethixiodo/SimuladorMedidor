572efd5279279870089323907539b5d8a0f396b0
Ethixiodo <73802838+Ethixiodo@users.noreply.github.com>
2021-06-06 23:36:10 -0400

    Add files via upload

---
+++ MedidorModelo/DAL/LecturaDALArchivos.cs
@@ -0,0 +1,85 @@
+﻿using MedidorModelo.DTO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorModelo.DAL
+{
+    public class LecturaDALArchivos : ILecturaDAL
+    {
+
+        private LecturaDALArchivos()
+        {
+
+        }
+        private static LecturaDALArchivos instancia;
+        public static ILecturaDAL GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new LecturaDALArchivos();
+            }
+            return instancia;
+        }
+        private static string url = Directory.GetCurrentDirectory();
+        private static string archivo = url + "/lecturas.txt";
+        public void IngresarLectura(Lectura lectura)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(lectura.NroMedidor + "|" + lectura.Fecha + "|" + lectura.ValorConsumo);
+                    writer.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public List<Lectura> ObtenerLecturas()
+        {
+            List<Lectura> lista = new List<Lectura>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(archivo))
+                {
+                    string texto = "";
+                    do
+                    {
+                        texto = reader.ReadLine();
+                        if (texto != null)
+                        {
+                            string[] arr = texto.Trim().Split('|');
+                            string[] palabrasFecha = arr[1].Split('-');     
+                            int nroMedidor = Convert.ToInt32(arr[0]);
+                            DateTime fecha = Convert.ToDateTime(arr[1]);
+                            double valorConsumo = Convert.ToDouble(arr[2]);
+                            Lectura lectura = new Lectura()
+                            {
+                                NroMedidor = nroMedidor,
+                                Fecha = fecha,
+                                ValorConsumo = valorConsumo
+                            };
+                            lista.Add(lectura);
+                        }
+
+                    } while (texto != null);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lista = null;
+            }
+            return lista;
+        }
+    }
+    }
+
+
---
+++ SimuladorMedidor/Program.cs
@@ -0,0 +1,151 @@
+﻿using MedidorModelo.DAL;
+using MedidorModelo.DTO;
+using Mensajero.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimuladorMedidor
+{
+    public class Program
+    {
+        private static ILecturaDAL lecturaDAL = LecturaDALArchivos.GetInstancia();
+        private static IMedidorDAL medidorDAL = MedidorDALObjeto.GetInstancia();
+
+        static bool Menu()
+        {
+            bool continuar = true;
+            Console.WriteLine("\nBienvenido al Mensajero");
+            Console.WriteLine(" 1. Ingresar Lecturas \n 2. Mostrar Lecturas \n 3. Revisar medidores existentes \n 0. Salir");
+            switch (Console.ReadLine().Trim())
+            {
+                case "1":
+                    IngresarLectura();
+                    break;
+                case "2":
+                    MostrarLectura();
+                    break;
+                case "3":
+                    MostrarMedidores();
+                    break;
+                case "0":
+                    continuar = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Ingrese de nuevo");
+                    break;
+            }
+            return continuar;
+
+        }
+        static void Main(string[] args)
+        {
+            
+
+            HebraServidor hebra = new HebraServidor();
+            Thread t = new Thread(new ThreadStart(hebra.Ejecutar));
+            t.IsBackground = true;
+            t.Start();
+            while (Menu()) ;
+
+     
+        }
+        static void IngresarLectura()
+        {
+            try
+            {
+                string txtnroMedidor;
+                string txtFecha;
+                string txtValorConsumo;
+                HebraCliente test = new HebraCliente();
+                do
+                {
+                    Console.WriteLine("Ingrese el número del medidor");
+                    txtnroMedidor = Console.ReadLine().Trim();
+                    if (test.validarMedidor(Convert.ToInt32(txtnroMedidor)))
+                    {
+                        do
+                        {
+
+                            Console.WriteLine("Ingrese la fecha");
+                            txtFecha = Console.ReadLine().Trim();
+                        } while ((txtFecha == string.Empty));
+
+                        do
+                        {
+
+                            Console.WriteLine("Ingrese el valor de consumo");
+                            txtValorConsumo = Console.ReadLine().Trim();
+                            txtValorConsumo = txtValorConsumo.Replace(".", ",");
+                        } while ((txtValorConsumo == string.Empty));
+                        int nroMedidor = Convert.ToInt32(txtnroMedidor);
+                        DateTime fecha = Convert.ToDateTime(txtFecha);
+                        double valorConsumo = Convert.ToDouble(txtValorConsumo);
+                        Lectura lectura = new Lectura()
+                        {
+                            NroMedidor = nroMedidor,
+                            Fecha = fecha,
+                            ValorConsumo = valorConsumo
+                        };
+                        lock (lecturaDAL)
+                        {
+                            lecturaDAL.IngresarLectura(lectura);
+                        }
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR");
+                    }
+                } while (txtnroMedidor == string.Empty);
+
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("\nFORMATO INVALIDO\n");
+            }
+
+        }
+
+        static void MostrarLectura()
+        {
+
+            List<Lectura> lecturas = null;
+            lock (lecturaDAL)
+            {
+                lecturas = lecturaDAL.ObtenerLecturas();
+                
+            };
+            for (int i = 0; i < lecturas.Count(); ++i)
+            {
+                Lectura actual = lecturas[i];
+                Console.WriteLine("{0}:\nNumero medidor:{1}\nFecha:{2}\nValor consumo:{3}\n--------o-------\n",
+                    i + 1, actual.NroMedidor, actual.Fecha, actual.ValorConsumo);
+            }
+        }
+
+        static void MostrarMedidores()
+        {
+            List<Medidor> medidores = null;
+            lock (medidorDAL)
+            {
+                medidores = medidorDAL.ObtenerMedidores();
+
+            };
+            Console.WriteLine("Medidores existentes:");
+            for (int i = 0; i < medidores.Count(); ++i)
+            {
+                Medidor actual = medidores[i];
+                Console.WriteLine("\nNumero medidor:{1}",
+                    i + 1, actual.NroMedidor);
+            }
+        }
+    }
+}
